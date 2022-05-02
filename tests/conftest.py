a68225dcca13070292059a8d0321dbef606fcee6
aMahanna <anthony.mahanna@gmail.com>
2022-05-02 17:02:04 -0400

    cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def pytest_addoption(parser: Any) -> None:
     parser.addoption("--port", action="store", default="8529")
     parser.addoption("--dbName", action="store", default="_system")
     parser.addoption("--username", action="store", default="root")
-    parser.addoption("--password", action="store", default="opensesame")
+    parser.addoption("--password", action="store", default="openSesame")
 
 
 def pytest_configure(config: Any) -> None:
